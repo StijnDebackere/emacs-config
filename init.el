e7759dbf175079b24c35f0f83793bce39285fa06
Stijn <debackere@strw.leidenuniv.nl>
2020-07-15 11:09:31 +0200

    Added visual-regexp-steroids for replacing strings

--- init.el
+++ init.el
@@ -688,16 +688,11 @@ point reaches the beginning or end of the buffer, stop there."
 ;; to get visual-regexp-steroids to work:
 ;; change vr--command-python-default python -> python3, no unicode errors
 ;; need to run python3 for *Packages* error
-;; (use-package visual-regexp-steroids
-;;   :bind
-;;   ("C-c r" . vr/replace)
-;;   ("C-c q" . vr/query-replace)
-;;   ("C-s" . vr/isearch-forward)
-;;   ("C-r" . vr/isearch-backward)
-;;   :custom
-;;   (vr/command-python
-;;    "python3 ~/.emacs.d/elpa/visual-regexp-steroids-20170222.253/regexp.py")
-;;   )
+(use-package visual-regexp-steroids
+  :bind
+  ("C-c r" . vr/replace)
+  ("C-c q" . vr/query-replace)
+  )
 
 
 ;;; Themes:
@@ -927,7 +922,7 @@ Hook this function into `TeX-after-compilation-finished-functions'."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(super-save company-math company-lua company-anaconda lua-mode all-the-icons-ivy-rich-mode all-the-icons-dired all-the-icons-ivy-rich powerline all-the-icons avy-zap company-auctex smartparens latex auctex tex material-theme anaconda-mode flycheck company multiple-cursors buffer-move winum magit exec-path-from-shell diminish use-package))
+   '(visual-regexp-steroids super-save company-math company-lua company-anaconda lua-mode all-the-icons-ivy-rich-mode all-the-icons-dired all-the-icons-ivy-rich powerline all-the-icons avy-zap company-auctex smartparens latex auctex tex material-theme anaconda-mode flycheck company multiple-cursors buffer-move winum magit exec-path-from-shell diminish use-package))
  '(smartparens-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
